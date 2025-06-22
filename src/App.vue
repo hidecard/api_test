74e054285e6abd8ae8065676ad2d4805aad4ba19
hidecard <hidecard1500@gmail.com>
2025-06-22 08:52:42 +0630

    day9

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,85 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div id="app">
+    <div class="container">
+        <div class="add">
+            <form action="">
+                <input placeholder="Add new task" type="text" name="" id="">
+                <button>Add Task</button>
+            </form>
+        </div>
+        <div class="update">
+            <form action="">
+                <label for="update">Update</label>
+                <input type="text" placeholder="Update task" name="" id="">
+                <button class="u">Update Task</button>
+                <button class="c">Cancel</button>
+            </form>
+        </div>
+        <div class="tabledata">
+            <table>
+                <tr>
+                    <th>Id</th>
+                    <th>Title</th>
+                    <th>Action</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>Hello</td>
+                    <td>
+                        <button class="btn1">Edit</button>
+                        <button class="btn2">Delete</button>
+                    </td>
+                </tr>
+            </table>
+        </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+   </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style>
+    .container{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        height: 100vh;
+    }
+    .add , .update , .tabledata{
+        margin: 10px;
+    }
+    .add button{
+        background-color: orangered;
+        color: white;
+        padding: 10px;
+        border: none;
+        border-radius: 5px;
+    }
+    .add input{
+        padding: 10px;
+        border-radius: 5px;
+    }
+    .update input{
+        padding: 10px;
+        border-radius: 5px;
+    }
+    .c , .btn2{
+        background-color: rgb(255, 0, 43);
+        color: white;
+        padding: 10px;
+        border: none;
+        border-radius: 5px;
+    }
+    .u , .btn1{
+        background-color: rgb(6, 192, 46);
+        color: white;
+        padding: 10px;
+        border: none;
+        border-radius: 5px;
+    }
+    table , tr ,td ,th{
+        border: 1px solid black;
+        padding: 10px 60px;
+        border-collapse: collapse;
+    }
+  </style>
--- src/main.js
+++ src/main.js
@@ -1,3 +1,13 @@
 import axios from 'axios'
 import Vue from 'vue'
 import App from './App.vue'
+
+Vue.config.productionTip = false;
+
+Vue.prototype.$axios = axios.create({
+    baseURL: 'http://127.0.0.1:8000/api'
+})
+
+new Vue({
+    render: h => h(App),
+}).$mount('#app')
